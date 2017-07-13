c5297f88a7d84cb660b45e606e4e23bbb3c9d810
aquastar <aquastar@qq.com>
2017-07-13 10:11:29 -0400

    update

--- cse.py
+++ cse.py
@@ -1,3 +1,5 @@
+import traceback
+
 import requests
 import json
 import os
@@ -16,14 +18,21 @@ query_list = ['drinking', 'alcohol', 'drunk party', 'booze', 'liquor', 'binge dr
               'anonymous', 'criminal', 'abuse', 'victim', 'violence', 'wrath', 'shot']
 
 for q in query_list:
+    print 'Query', q
     for p in xrange(1, page_lmt):
+        print 'Current page', p
         url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyCv94G0_nFK2WkPM12KhCuCJxh4sAIpFSc&cx=013228809236883522378:2t1dbtee42m&q={}&searchType=image&alt=json&start={}'.format(
             q, p)
 
-        r = requests.get(url)
-        rst = json.loads(r.content)['items']
+        try:
+            r = requests.get(url)
+            rst = json.loads(r.content)['items']
 
-        for _ in rst:
-            img_txt_list.append([_['link'], _['title']])
+            for _ in rst:
+                img_txt_list.append([_['link'], _['title']])
+        except:
+            print traceback.print_exc()
+            break
 
+print 'data size:', len(img_txt_list)
 pk.dump(img_txt_list, open(fname, 'wb'))
